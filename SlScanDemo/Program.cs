26f0f6392586e52a8e92e84a568d818ad42a9a8b
d.siamashka@godeltech.com <d.siamashka@godeltech.com>
2021-07-12 14:43:23 +0300

    sas token commit

--- SlScanDemo/Program.cs
+++ SlScanDemo/Program.cs
@@ -14,6 +14,7 @@ namespace SlScanDemo
         public static void Main(string[] args)
         {
             args[0] = "8sj+R0eymxz/IEDKWaCLc+DaSLHvRweLzbAeIc1/G6nSGQ8mf2xkqKRCMdVpMPup0TdmPVXfq9m48/MZxUhVmg==";
+            args[1] = "sp=r&st=2021-07-12T11:42:40Z&se=2021-07-12T19:42:40Z&spr=https&sv=2020-08-04&sr=c&sig=feJgisg4SL%2BQ1yaQfRDSsDnHuvlyHPaRv2fh27%2BrWas%3D"; // sas token
             CreateHostBuilder(args).Build().Run();
         }
 
